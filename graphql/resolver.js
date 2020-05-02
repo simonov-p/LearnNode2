75b9c1750fbc231e883e176a02a8002033eac78b
Petr <simonov-p@ya.ru>
2020-05-02 22:24:49 +0300

    96. Что такое GraphQL. 97. Настройка GraphQL. 98. Как работает GraphQL.

---
+++ graphql/resolver.js
@@ -0,0 +1,30 @@
+const users = [
+    {name: 'Igor', age: 33, email: 'igor@gmail.com'},
+    {name: 'Ann', age: 28, email: 'elena@gmail.com'}
+];
+
+module.exports = {
+    test() {
+        return {
+            count: Math.trunc(Math.random() * 10),
+            users
+        }
+    },
+    random({min, max, count}) {
+        const arr = [];
+        for (let i = 0; i < count; i++) {
+            const random = Math.random() * (max - min) + min;
+            arr.push(random);
+        }
+        return arr;
+    },
+    addTestUser({user: {name, email}}) {
+        const user = {
+            name,
+            email,
+            age: Math.ceil(Math.random() * 30)
+        };
+        users.push(user);
+        return user;
+    }
+};
---
+++ graphql/schema.js
@@ -0,0 +1,28 @@
+const {buildSchema} = require('graphql');
+
+module.exports = buildSchema(`
+    type User {
+        name: String!
+        age: Int!
+        email: String!
+    }
+
+    type TestType {
+        count: Int!
+        users: [User!]!
+    }
+    
+    type Query {
+        test: TestType!
+        random(min: Int!, max: Int!, count: Int!): [Float!]!
+    }
+    
+    input UserInput {
+        name: String!
+        email: String!
+    }
+    
+    type Mutation {
+        addTestUser(user: UserInput!): User!
+    }
+`);
