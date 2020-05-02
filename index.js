6687ad513ef296b9f671518f27c8cca17ab001c4
Petr <simonov-p@ya.ru>
2020-05-02 14:10:00 +0300

    Раздел 9: БД MySQL-REST API приложение. 84-87 Настройка роутера

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const path= require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const path = require('path')
+const todoRoutes = require('./routes/todo')
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api/todo', todoRoutes)
+
 
 app.use((req, res, next) => {
-    res.sendFile('/index.html');
-});
+    res.sendFile('S:\\study\\node\\udemy\\ch09_MySQL_REST_API\\public\\index.html')
+})
 
-app.listen(PORT);
+app.listen(PORT)
