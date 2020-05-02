280d0b107c78fc4cd0984bd704b1175194db6d09
Petr <simonov-p@ya.ru>
2020-05-02 20:13:20 +0300

    93. Получение списка задач.

--- routes/todo.js
+++ routes/todo.js
@@ -3,8 +3,16 @@ const Todo = require('../modules/todo');
 const router = Router();
 
 // Получение списка задач
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const todos = await Todo.findAll();
+        res.status(200).json(todos);
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
 });
 
 // Создание новой задачи
